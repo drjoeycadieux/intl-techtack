98209970f617c7870df9427cd43653b7652c32bd
CodeSandbox <support@codesandbox.io>
2023-11-30 15:52:38 +0000

    Initial commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <main>
+      <div className="_Header">
+        <h1>International Techtack-Technologies</h1>
+      </div>
+    </main>
+  );
+}
---
+++ app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="_Header">
+        <h1>International Techtack-Technologies</h1>
+      </div>
+    </main>
+  );
+}
