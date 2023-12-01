5a3178bbf7bd2933f98e3736c6dab1deab812305
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-12-01 16:47:07 +0000

    Delete Header.tsx and header.tsx

--- app/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <main>
-      <div className="_Header">
-        <h1>International Techtack-Technologies</h1>
-      </div>
-    </main>
-  );
-}
--- app/components/header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <div className="_Header">
-        <h1>International Techtack-Technologies</h1>
-      </div>
-    </main>
-  );
-}
